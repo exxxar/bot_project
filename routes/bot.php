c3d52839f12cd5c2df4786e0d0007c9a99466715
exxxar <exxxar@gmail.com>
2020-08-25 23:55:28 +0300

    first

--- routes/bot.php
+++ routes/bot.php
@@ -9,7 +9,7 @@ $this->hears("/test ([0-9]+) ([a-zA-Z]+)", function ($bot, ...$d) {
 });
 
 
-$this->hears("/start|.*Попробовать снова|*.Главное меню", function ($bot) {
+$this->hears("/start|*.Главное меню", function ($bot) {
     $bot->getMainMenu("Добро пожаловать в модельное агенство Lotus!");
    // $bot->reply("conversation started");
 });
